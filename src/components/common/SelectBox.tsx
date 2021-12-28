0d037195866f1f40d6d7e88a294865464c82eb16
전병길 <jeonbg@kakao.com>
2021-12-29 03:16:52 +0900

    feature : SelectBox

--- src/components/common/SelectBox.tsx
+++ src/components/common/SelectBox.tsx
@@ -1,8 +1,35 @@
-const SelectBox = () => {
+import { ChangeEvent, memo } from 'react';
+
+interface Props {
+  id: string;
+  name: string;
+  filterList: string[];
+  selectedFilter?: string[];
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SelectBox = ({
+  id,
+  name,
+  filterList,
+  selectedFilter,
+  onChange,
+}: Props) => {
   return (
-    <div>
-      <p>SelectBox</p>
-    </div>
+    <span className='select-box'>
+      <input type='text' list={id} name={name} onChange={onChange} />
+      <datalist id={id}>
+        {filterList?.map((list, index) => {
+          const isActive = selectedFilter?.includes(list);
+          if (isActive) return;
+          return (
+            <option key={`${list}-${index}`} value={list}>
+              {list}
+            </option>
+          );
+        })}
+      </datalist>
+    </span>
   );
 };
 
