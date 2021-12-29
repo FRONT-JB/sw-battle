73d45affa1c8931bfba49a54c3dc225f114d5954
전병길 <jeonbg@kakao.com>
2021-12-29 19:16:55 +0900

    fix (#9) : redux persist fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import persistStore from 'redux-persist/es/persistStore';
 import App from '~/components/App';
 import store from './store';
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
-import { commentApi } from './../api/comment';
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import persistReducer from 'redux-persist/es/persistReducer';
+import { persistReducer } from 'redux-persist';
 import sessionStorage from 'redux-persist/es/storage/session';
 import { boardApi } from '~/api/board';
+import { commentApi } from '~/api/comment';
 import { monsterApi } from '~/api/monster';
 import rootReducer from './reducer';
 
