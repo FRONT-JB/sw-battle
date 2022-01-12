d3ff7e34c1abb508449c73ef60e5a837d54effe5
전병길 <jeonbg@kakao.com>
2022-01-12 21:08:29 +0900

    refactor : defense list scroll

--- src/components/defense/DefenseList.tsx
+++ src/components/defense/DefenseList.tsx
@@ -1,17 +1,19 @@
+import { memo, useEffect, useMemo, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
+import classNames from 'classnames';
 import { useGetBoardListQuery } from '~/api/board';
 import { ROUTE_PATH } from '~/routes/path';
 import { handleTimeForToday } from '~/utils/time';
 import MonsterImage from '../common/MonsterImage';
-import classNames from 'classnames';
-import { useEffect, useRef } from 'react';
+import { throttle } from 'lodash';
 
 const DefenseList = () => {
   const { data: boardList } = useGetBoardListQuery({});
   const { id: boardId } = useParams();
   const navigate = useNavigate();
-  const isNotNullList = !!boardList?.length;
   const listRef = useRef<HTMLDivElement>(null);
+  const isNotNullList = !!boardList?.length;
+
   const handleNavigate = (id: number) => {
     navigate(`${ROUTE_PATH.DETAIL}/${id}`);
   };
@@ -20,13 +22,35 @@ const DefenseList = () => {
     if (!boardId) {
       listRef?.current?.scrollTo(0, 0);
     }
+    listRef.current?.addEventListener(
+      'scroll',
+      throttle(handleScrollActive, 1000),
+    );
+    return () => {
+      listRef.current?.removeEventListener(
+        'scroll',
+        throttle(handleScrollActive, 1000),
+      );
+    };
   }, [boardId]);
+
+  useEffect(() => {}, []);
+
+  const handleScrollActive = () => {
+    if (!listRef || !listRef.current) return;
+    const isScroll = listRef.current.scrollTop > 0;
+    if (isScroll) {
+      listRef.current.classList.add('isScroll');
+    } else {
+      listRef.current.classList.remove('isScroll');
+    }
+  };
 
   return (
     <div className='defense-list' ref={listRef}>
       {!isNotNullList && <p style={{ height: '100%' }}>No Result</p>}
       {isNotNullList && (
-        <ul className='defense-list-card'>
+        <ul className={classNames('defense-list-card')}>
           {boardList?.map(({ id, keyword, content, user, created_at }) => (
             <li
               className={classNames('defense-list-card__list', {
@@ -59,4 +83,4 @@ const DefenseList = () => {
   );
 };
 
-export default DefenseList;
+export default memo(DefenseList);
