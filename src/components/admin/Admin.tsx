d1c6fbd4f3d46b900d0ffd8c067dae3f702bc7a8
전병길 <jeonbg@kakao.com>
2022-01-31 23:17:34 +0900

    feature : Admin Page
    1. Add Recycle Component
    2. Fix Admin Page Component

---
+++ src/components/admin/Admin.tsx
@@ -0,0 +1,13 @@
+import { Recycle } from '../recycle';
+import { User } from '../user';
+
+const Admin = () => {
+  return (
+    <div>
+      <User />
+      <Recycle />
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/components/recycle/Recycle.tsx
@@ -0,0 +1,12 @@
+const Recycle = () => {
+  return (
+    <div className='recycle'>
+      <b className='title'>지운 리스트</b>
+      <div className='not-found'>
+        <span className='not-found__message'>No Result</span>
+      </div>
+    </div>
+  );
+};
+
+export default Recycle;
